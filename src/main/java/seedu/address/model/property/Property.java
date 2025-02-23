ebd1a083e931f6820009668adf2925667f0f5ca0
Ryuse <Leemuel01234@gmail.com>
2025-02-24 01:04:24 +0800

    Add Owner and Property List

---
+++ src/main/java/seedu/address/model/property/Property.java
@@ -0,0 +1,49 @@
+package seedu.address.model.property;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+import seedu.address.model.person.Person;
+import seedu.address.model.tag.Tag;
+
+/*
+ * A sample property class
+ * */
+public class Property{
+    private String name;
+    private String postalCode;
+    private String houseNumber;
+    private String unitNumber;
+    private HashSet<Tag> tags = new HashSet<>();
+    private ArrayList<Person> owners;
+
+    public Property(String postalCode, String houseNumber, String unitNumber){
+        this.postalCode = postalCode;
+        this.houseNumber = houseNumber;
+        this.unitNumber = unitNumber;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getPostalCode(){
+        return postalCode;
+    }
+
+    public String getHouseNumber(){
+        return houseNumber;
+    }
+
+    public String getUnitNumber(){
+        return unitNumber;
+    }
+
+    public HashSet<Tag> getTags(){
+        return tags;
+    }
+
+    public ArrayList<Person> getOwners(){
+        return owners;
+    }
+}
---
+++ src/main/java/seedu/address/ui/PropertyListPanel.java
@@ -0,0 +1,60 @@
+package seedu.address.ui;
+
+import java.util.ArrayList;
+import java.util.logging.Logger;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.Region;
+import seedu.address.commons.core.LogsCenter;
+import seedu.address.model.property.Property;
+
+/**
+ * Panel containing the list of properties.
+ */
+public class PropertyListPanel extends UiPart<Region> {
+    private static final String FXML = "PropertyListPanel.fxml";
+    private final Logger logger = LogsCenter.getLogger(PropertyListPanel.class);
+
+    @FXML
+    private ListView<Property> propertyListView;
+
+    /**
+     * Creates a {@code PropertyListPanel} with the given {@code ObservableList}.
+     */
+    public PropertyListPanel(ObservableList<Property> propertyList) {
+        super(FXML);
+        propertyListView.setItems(propertyList);
+        propertyListView.setCellFactory(listView -> new PropertyListViewCell());
+    }
+
+
+    public static ObservableList<Property> getSamplePropertyData(){
+        ArrayList<Property> testData = new ArrayList<>();
+        Property sampleProperty = new Property("123456", "11A", "11-11111");
+        testData.add(sampleProperty);
+
+        return FXCollections.observableList(testData);
+    }
+
+    /**
+     * Custom {@code ListCell} that displays the graphics of a {@code Property} using a {@code PropertyCard}.
+     */
+    class PropertyListViewCell extends ListCell<Property> {
+        @Override
+        protected void updateItem(Property property, boolean empty) {
+            super.updateItem(property, empty);
+
+            if (empty || property == null) {
+                setGraphic(null);
+                setText(null);
+            } else {
+                setGraphic(new PropertyCard(property, getIndex() + 1).getRoot());
+            }
+        }
+    }
+
+}
